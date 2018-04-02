8754ec3cdf5c51d61a7df10a3cc8ea62c614f0b2
Madi Atkins <madiatk@umich.edu>
2018-04-02 14:17:07 -0400

    adding id to the images on memories page to write js to work

--- RosalitaPortfolio/js/main.js
+++ RosalitaPortfolio/js/main.js
@@ -1,10 +1,12 @@
+// about page image hover
+
 $(document).ready(function() {
 	$('.preview').on('mouseover focus', function() {
-		$('#image').text($(this).attr('alt'));
-		$('#image').css('background-image', 'url("'+ $(this).attr('src')+ '")');
+		$('.psst').text($(this).attr('alt'));
+		$('.psst').css('background-image', 'url("'+ $(this).attr('src')+ '")');
 	$('.preview').on('mouseleave blur', function() {
-		$('#image').text("Hover over an image below!");
-		$('#image').css('background-image', '');
+		$('.psst').text("Hover over an image below!");
+		$('.psst').css('background-image', '');
 })
 	})
 })
