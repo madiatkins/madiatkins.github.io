6d955e742e36bd1b5098a9b8ab9666f0df4ba6d5
Madi Atkins <madiatk@umich.edu>
2018-04-02 22:08:42 -0400

    hover over memories images for alt text DONE

--- RosalitaPortfolio/js/main.js
+++ RosalitaPortfolio/js/main.js
@@ -1,13 +1,57 @@
 // about page image hover
 
 $(document).ready(function() {
-	$('.preview').on('mouseover focus', function() {
-		$('.psst').text($(this).attr('alt'));
-		$('.psst').css('background-image', 'url("'+ $(this).attr('src')+ '")');
-	$('.preview').on('mouseleave blur', function() {
-		$('.psst').text("Hover over an image below!");
-		$('.psst').css('background-image', '');
-})
+ // Wrap every .img in a div
+  // 1. For all items with class .img...
+  // 2. Wrap them in a div
+	$(".preview").wrap('<div class="alt-wrap"/>');
+
+	// Add alt text after each .img
+  // 1. For all items with class .img...
+  // 2. Run a function for each of them...
+  // 3. To add a p element after it
+  // 5. Containing that element's alt text
+	$(".preview").each(function() {
+		$(this).after('<p class="alt">' + $(this).attr('alt') + '</p>');
 	})
 })
+// CSS shows/hides the alt text on hover
+
+
+
+
+
+
+
+
+
+// $("memories.html").ready(function() {}
+// 	$(".xmas").click(function () {
+// 	    $(".layout").css("display", "block");
+// 	    $(".layout").text($(this).attr('alt')); 
+// 	})
+// })
+
+
+// $("#preview").on("click", function() {
+// 	 $('.xmas').text($(this).attr("alt"));
+// 	});
+
+
+
+
+
+
+
+// $(document).ready(function() {
+// 	console.log("check")
+// 	$('#preview').on('mouseover', function() {
+// 		$(this).text($(this).attr('alt'));
+// 		$(this).css('background-image', 'url("'+ $(this).attr('src')+ '")');
+// 	$('#preview').on('mouseleave', function() {
+// 		$(this).text("Hover over an image below!");
+// 		$(this).css('background-image', '');
+// })
+// 	})
+// })
 
